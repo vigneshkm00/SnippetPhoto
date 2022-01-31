6d88061e6f55d8f83262b1a8a6ee94b999ba797d
vmothila <Pulsar@9962>
2022-01-31 16:14:30 +0530

    Changes in Photo Path

--- app/src/main/java/com/example/snippetphoto/MainActivity.java
+++ app/src/main/java/com/example/snippetphoto/MainActivity.java
@@ -1,29 +1,45 @@
 package com.example.snippetphoto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
+    TextView textView;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
         imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.imagePath);
         findViewById(R.id.Gbutton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent pickPhoto = new Intent(Intent.ACTION_PICK,
                         android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(pickPhoto , 1);//one can be replaced with any action code
+                startActivityForResult(pickPhoto , 1);
 
             }
         });
@@ -31,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(takePicture, 0);//zero can be replaced with any action code (called requestCode)
+                startActivityForResult(takePicture, 0);
             }
         });
     }
@@ -42,7 +58,12 @@ public class MainActivity extends AppCompatActivity {
                 if(resultCode == RESULT_OK){
                     Bundle extras = imageReturnedIntent.getExtras();
                     Bitmap imageBitmap = (Bitmap) extras.get("data");
-                    imageView.setImageBitmap(imageBitmap);
+                    //Storing image in gallery
+                    String savedImageURL = MediaStore.Images.Media.insertImage(getContentResolver(),imageBitmap,"Snippet","example");
+                    //Retrieving image from gallery using URL
+                    Uri savedImageURI = Uri.parse(savedImageURL);
+                    imageView.setImageURI(savedImageURI);
+                    textView.setText(savedImageURL);//Image URL
                 }
 
                 break;
@@ -50,10 +71,31 @@ public class MainActivity extends AppCompatActivity {
                 if(resultCode == RESULT_OK){
                     Uri selectedImage = imageReturnedIntent.getData();
                     imageView.setImageURI(selectedImage);
+                    String path = selectedImage.getPath();
+                    textView.setText(path);//Image URL
                 }
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + requestCode);
         }
+    }
+    private String saveToInternalStorage(Bitmap bitmapImage){
+        ContextWrapper contextWrapper = new ContextWrapper(getApplicationContext());
+        File directory = contextWrapper.getDir("imageDir", Context.MODE_PRIVATE);
+        File mypath = new File(directory,"ex.jpg");
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            bitmapImage.compress(Bitmap.CompressFormat.PNG,100,fos);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return directory.getAbsolutePath();
     }
 }
